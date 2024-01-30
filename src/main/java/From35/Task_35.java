83de1d6de5a482de623dd91b85a441f83b4ec5f8
PisarevskiyRE <daimon--@live.ru>
2024-01-30 16:59:32 +0300

    leetcode

--- src/main/java/From35/Task_35.java
+++ src/main/java/From35/Task_35.java
@@ -3,16 +3,28 @@ package From35;
 import java.io.Console;
 
 public class Task_35 {
-    public int searchInsert(int[] nums, int target) {
+    public static int searchInsert(int[] a, int target) {
+        int left = -1;
+        int right = a.length;
 
-        return 0;
+        while (right - left > 1) {
+            int mid = left + ((right-left) >> 1 );
+            if (a[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
     }
 
 
     public static void main(String[] args){
 
 
-        System.console().writer().println("123");
+        int result = searchInsert(new int[]{1,3,5,6}, 0);
+
+        System.out.println(result);
 
 
     }
