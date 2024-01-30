d81814011e449872ec3fcee8909cb12c80d1a9e2
PisarevskiyRE <daimon--@live.ru>
2024-01-30 14:56:40 +0300

    first commit

--- src/main/java/From35/Task_35.java
+++ src/main/java/From35/Task_35.java
@@ -1,4 +1,19 @@
 package From35;
 
+import java.io.Console;
+
 public class Task_35 {
+    public int searchInsert(int[] nums, int target) {
+
+        return 0;
+    }
+
+
+    public static void main(String[] args){
+
+
+        System.console().writer().println("123");
+
+
+    }
 }
